5cfc3f97a1afe3aea94ed1798765fd0fc4b3a517
Shourya Gupta <121602573+shourya13x@users.noreply.github.com>
2024-06-30 17:15:46 +0530

    Update Erc20.sol

--- Erc20.sol
+++ Erc20.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CustomToken is ERC20, Ownable {
+    constructor() ERC20("CrazyToken", "Crz") Ownable(msg.sender) {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+    function issueTokens(address beneficiary, uint256 quantity) public onlyOwner {
+        _mint(beneficiary, quantity);
+    }
+    function destroyTokens(uint256 quantity) public {
+        _burn(msg.sender, quantity);
+    }
+}
